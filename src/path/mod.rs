373219fd8163b953d970a9224eeb9efce16c292d
Paul Montag <paul.david.montag@gmail.com>
2024-03-14 08:05:44 -0500

    Working on lexor for traversing packet

--- src/path/mod.rs
+++ src/path/mod.rs
@@ -1,3 +1,19 @@
+const TOKEN_DOT: char = '.';
+const TOKEN_RECURSIVE: char = '~';
+const TOKEN_ARRAY_OPEN: char = '[';
+const TOKEN_ARRAY_CLOSE: char = ']';
+const TOKEN_FIRST_OPEN: char = '{';
+const TOKEN_FIRST_CLOSE: char = '}';
+const TOKEN_FIRST_SEP: char = '|';
+const TOKEN_MULTI_OPEN: char = '(';
+const TOKEN_MULTI_CLOSE: char = ')';
+const TOKEN_MULTI_SEP: char = ',';
+const TOKEN_WILDCARD: char = '*';
+const TOKEN_REGEX: char = '/';
+
+const TOKEN_ESCAPE: char = '\\';
+const TOKEN_STRING_WRAP: char = '"';
+
 pub struct PathLexor<'a> {
     path: &'a str,
     head: usize,
@@ -9,55 +25,86 @@ pub fn new_path_lexor<'a>(path: &'a str) -> PathLexor<'a> {
 
 impl<'a> PathLexor<'a> {
     pub fn token(&mut self) -> Option<&'a str> {
-        // consume any whitespace, we don't care
-        // about whitespace
-        self.consume_whitespace();
-        None
-    }
+        if self.head >= self.path.len() {
+            return None;
+        }
 
-    // consume_whitespace will consume any whitespace that exists in the key,
-    // if you are a monster and create field values with whitespace in them
-    // you will have to escape it or wrap it in double quotes. Also you
-    // should see a shrink because god knows what other demented things you
-    // are doing.
-    fn consume_whitespace(&mut self) {
-        let c = self.path.chars().skip(self.head);
-        for char in c {
+        let c = self.path[self.head..].char_indices();
+        let mut tok: Option<&str> = None;
+        let mut next_index = self.head;
+        let mut escape_next = false;
+        let mut escape_all = false;
+
+        'charloop: for (i, char) in c {
+            let index = i + self.head;
+
             match char {
-                '\u{0009}' => self.head += 1, // character tabulation
-                '\u{000A}' => self.head += 1, // line feed
-                '\u{000B}' => self.head += 1, // line tabulation
-                '\u{000C}' => self.head += 1, // form feed
-                '\u{000D}' => self.head += 1, // carriage return
-                '\u{0020}' => self.head += 1, // space
-                '\u{0085}' => self.head += 1, // next line
-                '\u{00A0}' => self.head += 1, // no-break space
-                '\u{1680}' => self.head += 1, // ogham space mark
-                '\u{180E}' => self.head += 1, // mongolian vowel separator
-                '\u{2000}' => self.head += 1, // en quad
-                '\u{2001}' => self.head += 1, // em quad
-                '\u{2002}' => self.head += 1, // en space
-                '\u{2003}' => self.head += 1, // em space
-                '\u{2004}' => self.head += 1, // three-per-em space
-                '\u{2005}' => self.head += 1, // four-per-em space
-                '\u{2006}' => self.head += 1, // six-per-em space
-                '\u{2007}' => self.head += 1, // figure space
-                '\u{2008}' => self.head += 1, // punctuation space
-                '\u{2009}' => self.head += 1, // thin space
-                '\u{200A}' => self.head += 1, // hair space
-                '\u{200B}' => self.head += 1, // zero width space
-                '\u{200C}' => self.head += 1, // zero width non-joiner
-                '\u{200D}' => self.head += 1, // zero width joiner
-                '\u{2028}' => self.head += 1, // line separator
-                '\u{2029}' => self.head += 1, // paragraph separator
-                '\u{202F}' => self.head += 1, // narrow no-break space
-                '\u{205F}' => self.head += 1, // medium mathematical space
-                '\u{2060}' => self.head += 1, // word joiner
-                '\u{3000}' => self.head += 1, // ideographic space
-                '\u{FEFF}' => self.head += 1, // zero width non-breaking space
-                _ => return,
+                TOKEN_DOT | TOKEN_WILDCARD | TOKEN_RECURSIVE | TOKEN_ARRAY_OPEN
+                | TOKEN_ARRAY_CLOSE | TOKEN_FIRST_OPEN | TOKEN_FIRST_CLOSE | TOKEN_MULTI_OPEN
+                | TOKEN_MULTI_CLOSE | TOKEN_FIRST_SEP | TOKEN_MULTI_SEP => {
+                    // if the previous token was an escape token, we just want to add this to the
+                    // existing token
+                    if escape_next || escape_all {
+                        tok = Some(&self.path[self.head..index + char.len_utf8()]);
+                        next_index = index + char.len_utf8();
+                        escape_next = false;
+                        continue 'charloop;
+                    }
+
+                    // check the token, if there is a value in there we consumed
+                    // an identifier first, so we need to return that. We keep
+                    // keep track of the previous_index so that tokens greater
+                    // than a single byte can be used.
+                    if let Some(_) = tok {
+                        break 'charloop;
+                    }
+
+                    // otherwise we are starting with a token, so lets return that
+                    tok = Some(&self.path[self.head..index + char.len_utf8()]);
+                    next_index = index + char.len_utf8();
+                    break 'charloop;
+                }
+                TOKEN_ESCAPE => {
+                    // collect the escapes so we can return something if the input is "\\\\\\\"
+                    tok = Some(&self.path[self.head..index + char.len_utf8()]);
+                    escape_next = true;
+                }
+                TOKEN_STRING_WRAP | TOKEN_REGEX => {
+                    // if the previous character was an escape we can ignore
+                    // the double quote
+                    if escape_next {
+                        escape_next = false;
+                        continue;
+                    }
+
+                    // invert escape to toggle on and off escaping all
+                    escape_all = !escape_all;
+
+                    if !escape_all {
+                        // after we turn it off collect the token, without
+                        // grabbing the ending "
+                        tok = Some(&self.path[self.head..index + char.len_utf8()]);
+                        next_index = index + char.len_utf8();
+                        break 'charloop;
+                    }
+
+                    // if we are just starting we want to collect the previous
+                    // token if it exists
+                    if let Some(_) = tok {
+                        break 'charloop;
+                    }
+                }
+                _ => {
+                    escape_next = false;
+                    tok = Some(&self.path[self.head..index + char.len_utf8()]);
+                    next_index = index + char.len_utf8();
+                }
             }
         }
+
+        // move the head forward to the last seen index
+        self.head = next_index;
+        tok
     }
 }
 
@@ -70,9 +117,10 @@ mod test {
             let mut toks = vec![];
             let expected: Vec<&str> = vec![$($args),*];
 
-            let tok = p.token();
-            while let Some(tok) = tok {
-                toks.push(tok);
+            let mut tok = p.token();
+            while let Some(t) = tok {
+                toks.push(t);
+                tok = p.token();
             }
 
             assert_eq!(toks, expected)
@@ -81,7 +129,40 @@ mod test {
 
     #[test]
     fn test_lexor() {
-        test_lexor!("    ",);
-        test_lexor!("I am a fish", "I", "am", "a", "fish");
+        test_lexor!("Im.am a.fish", "Im", ".", "am a", ".", "fish");
+        test_lexor!(" ", " ");
+        test_lexor!(
+            "labels.{hostname|host}",
+            "labels",
+            ".",
+            "{",
+            "hostname",
+            "|",
+            "host",
+            "}"
+        );
+        test_lexor!("labels\\.hostname", "labels\\.hostname");
+        test_lexor!(r#""labels.hostname""#, "\"labels.hostname\"");
+        test_lexor!(r#""some\"thing""#, r#""some\"thing""#);
+        test_lexor!(
+            r#""one""two""three""four""#,
+            "\"one\"",
+            "\"two\"",
+            "\"three\"",
+            "\"four\""
+        );
+        test_lexor!(
+            "€€€.€€€.€.asdf.asdf",
+            "€€€",
+            ".",
+            "€€€",
+            ".",
+            "€",
+            ".",
+            "asdf",
+            ".",
+            "asdf"
+        );
+        test_lexor!("/.*/.something", "/.*/", ".", "something");
     }
 }
